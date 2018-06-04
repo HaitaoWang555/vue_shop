2e74ce5349a76874b8da6379db66bd51a270f926
wanghaitao <15124505701@163.com>
2018-06-04 16:57:30 +0800

    添加登录页路由

---
+++ shop_app/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+    <h2>登录页</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page',
+  props: {
+
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+  @import './css/login.css';
+</style>
