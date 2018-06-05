fad343747a76f64ca263e46387b356065ac33a77
wanghaitao <15124505701@163.com>
2018-06-05 17:03:37 +0800

    登录页样式

--- shop_app/src/views/Login.vue
+++ shop_app/src/views/Login.vue
@@ -1,6 +1,32 @@
 <template>
-  <div class="page">
-    <h2>登录页</h2>
+  <div class="login">
+    <header class="header">
+      <em class="logo"></em>
+      <h4>小米账号登录</h4>
+    </header>
+    <section class="loginForm">
+      <form action="">
+        <label for="user" class="forUser"><input type="text" name="user" id="username" placeholder="邮箱/手机号码/小米ID"></label>
+        <label for="password" class="forPassword"><input type="password" name="password" id="passoord" placeholder="密码"><div class="visible"><i class="icon iconfont icon-kanjianmima-"></i></div></label>
+        <div class="errorInfo"><div><i class="icon iconfont icon-error"></i><span class="error-con">请输入帐号</span></div></div>
+        <div class="btnWrap"><input type="submit" id="loginBtn" class="commonBtn" value="登录"></div>
+      </form>
+    </section>
+    <section class="otherPanel">
+      <div class="changeLogin"><input type="text" class="commonBtn" value="手机短信登录/注册"></div>
+      <div class="reverse"><a href=""><span>立即注册</span></a> | <a href=""><span>忘记密码</span></a></div>
+      <div class="otherLogin">
+        <h4>其它方式登录</h4>
+        <ul class="iconLogin ignore"><li><i></i></li><li><i></i></li><li><i></i></li></ul>
+      </div>
+    </section>
+    <footer>
+      <ul>
+        <li>简体<span>|</span></li>
+        <li>常见问题<span>|</span></li>
+        <li>隐私政策</li>
+      </ul>
+    </footer>
   </div>
 </template>
 
