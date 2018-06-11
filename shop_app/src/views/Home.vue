aff6a8207466d9ca50c136fb4778044305d4fdfc
wanghaitao <15124505701@163.com>
2018-06-11 11:28:59 +0800

    首页header

--- shop_app/src/views/Home.vue
+++ shop_app/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <!-- header -->
+    <header>
+      <div class="searchLogo"><img src="../assets/images/searchlogo.png"></div>
+      <div class="search"><i class="iconfont icon-search"></i><span>搜索商品名称</span></div>
+      <div class="login"><i class="iconfont icon-people"></i></div>
+    </header>
     <!-- nav -->
     <!-- header -->
     <!-- shoplist -->
