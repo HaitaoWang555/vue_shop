df0d3dfb7ad239bc2651e5165b5b4917892c520d
wanghaitao <15124505701@163.com>
2018-06-26 15:58:07 +0800

    Goodsview

---
+++ shop_app/src/components/product/Goodsview.vue
@@ -0,0 +1,56 @@
+/* Goodsview vue组件 */
+<template>
+  <div class="goodsView" v-if="goodsview">
+    <div class="goodsName">{{goodsview.goodsName}}</div>
+    <div class="goodsBrief" v-html="goodsview.goodsBrief"></div>
+    <div class="goodsPrice">
+      <van-icon name="balance-pay" color="#ff6700"/>
+      <div class="curPrice">{{goodsview.goodsPrice}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goodsview',
+  props: {
+    goodsview: {
+      default: null
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+.goodsView {
+  background: #fff;
+  text-align: left;
+  padding: 10px 16px 0;
+}
+.goodsName {
+  font-size: 20px;
+  line-height: 28px;
+}
+.goodsBrief {
+  font-size: 14px;
+  line-height: 18px;
+  color: rgba(0,0,0,.54);
+}
+.goodsPrice {
+  padding: 10px 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 24px;
+}
+.goodsPrice .curPrice {
+  color: #ff6700;
+  font-weight: 700;
+}
+</style>
--- shop_app/src/views/Product.vue
+++ shop_app/src/views/Product.vue
@@ -1,33 +1,41 @@
 /* Product vue组件 */
 <template>
   <div class="product">
-    <ProductHeader />
-    <Swiper :msg="swiperImages"/>
-    <GoodsAction />
+    <Loading class="isFooter" v-if="loading" />
+    <template  v-if="!loading">
+      <ProductHeader />
+      <Swiper :msg="swiperImages"/>
+      <Goodsview :goodsview="goodsview"/>
+      <GoodsAction />
+    </template>
   </div>
 </template>
 
 <script>
+import Loading from '@/components/Loading.vue'
 import Swiper from '@/components/Swiper.vue'
 import ProductHeader from '@/components/product/ProductHeader.vue'
+import Goodsview from '@/components/product/Goodsview.vue'
 import GoodsAction from '@/components/product/GoodsAction.vue'
 import bus from '@/bus.js'
 export default {
   name: 'product',
   data () {
     return {
-      swiperImages: null
+      loading: true,
+      swiperImages: null,
+      goodsview: null
     }
   },
   components: {
+    Loading,
     Swiper,
     ProductHeader,
+    Goodsview,
     GoodsAction
   },
   beforeRouteEnter (to, from, next) {
-    next(vm => {
-      bus.$emit('loading', true)
-    })
+    next(bus.$emit('loading', true))
   },
   created () {
     this.$NProgress.start()
@@ -40,6 +48,8 @@ export default {
     getProduct () {
       this.$fetch('product').then((res) => {
         this.swiperImages = res.data.swiperImages
+        this.goodsview = res.data.overview
+        this.loading = false
         this.$NProgress.done()
       }).catch((err) => {
         console.log(err)
@@ -49,6 +59,11 @@ export default {
 }
 </script>
 
+<style scoped>
+.product .isFooter {
+  margin-top: 50px!important;
+}
+</style>
 <style>
 /* Product 组件下轮播样式 */
 .product .van-swipe img{
