8b451f52b29f57e6c8fdceba97bdea86224922d0
wanghaitao <15124505701@163.com>
2018-07-10 16:47:28 +0800

    user

--- shop_app/src/views/User.vue
+++ shop_app/src/views/User.vue
@@ -1,13 +1,42 @@
 /* User vue组件 */
 <template>
   <div class="user">
-    User vue组件
+    <header>
+      <div class="imgWrap"><img src="../assets/images/avatar.png"></div>
+      <div class="content">登录/注册</div>
+    </header>
+    <div class="userContent">
+      <div class="order cell"><span>我的订单</span><span>全部订单</span></div>
+      <ul class="tabrouter">
+        <li><i class="icon dfk"></i><span>代付款</span></li>
+        <li><i class="icon dsh"></i><span>待收货</span></li>
+        <li><i class="icon thx"></i><span>退换修</span></li>
+      </ul>
+      <DividerLine/>
+      <div class="tabLink member"><i class="icon"></i><div class="cell border-bottom "><span>会员中心</span></div></div>
+      <div class="tabLink wallet"><i class="icon"></i><div class="cell"><span>我的优惠</span></div></div>
+      <DividerLine/>
+      <div class="tabLink service"><i class="icon"></i><div class="cell border-bottom"><span>服务中心</span></div></div>
+      <div class="tabLink mihome"><i class="icon"></i><div class="cell"><span>小米之家</span></div></div>
+      <DividerLine/>
+      <div class="tabLink fcode"><i class="icon"></i><div class="cell"><span>F码通道</span></div></div>
+      <DividerLine/>
+      <div class="tabLink setting"><i class="icon"></i><div class="cell"><span>设置</span></div></div>
+    </div>
   </div>
 </template>
 
 <script>
+import bus from '@/bus.js'
+import DividerLine from '@/components/DividerLine.vue'
 export default {
   name: 'user',
+  components: {
+    DividerLine
+  },
+  beforeRouteEnter (to, from, next) {
+    next(bus.$emit('loading', true))
+  },
   created () {
     this.$NProgress.start()
     this.over()
@@ -26,13 +55,5 @@ export default {
 </script>
 
 <style scoped>
-.user {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  min-height: 800px;
-  background-color:aliceblue;
-}
+@import "./css/user.css";
 </style>
