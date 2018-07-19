9f057c9c10b452a9269d778f095174ecf1ec37eb
wanghaitao <15124505701@163.com>
2018-07-19 10:01:49 +0800

    chunksSortMode manual

--- shop_app/vue.config.js
+++ shop_app/vue.config.js
@@ -1,5 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/vue_shop/shop_app/dist/'
-    : '/'
+    : '/',
+    configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        chunksSortMode: 'manual'
+      })
+    ]
+  }
 }
