390e975c8cf2a7122ffbc5cddc4bb860028dd39d
wanghaitao <15124505701@163.com>
2018-07-19 11:13:26 +0800

    build

--- shop_app/vue.config.js
+++ shop_app/vue.config.js
@@ -1,13 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/vue_shop/shop_app/dist/'
-    : '/',
-    configureWebpack: {
-    plugins: [
-      new HtmlWebpackPlugin({
-        chunksSortMode: 'manual'
-      })
-    ]
-  }
+    : '/'
 }
